4e9a291c16f1e3f9bca9029d0d2018d15b1b1fc2
i-m-vivek <vivek.mittal051@gmail.com>
2021-07-22 17:19:16 +0530

    integrate tiago

--- gibson2/envs/env_base.py
+++ gibson2/envs/env_base.py
@@ -193,6 +193,8 @@ class BaseEnv(gym.Env):
             robot = Fetch(self.config)
         elif self.config['robot'] == 'Locobot':
             robot = Locobot(self.config)
+        elif self.config['robot'] == 'Tiago_Single': 
+            robot = Tiago_Single(self.config)
         else:
             raise Exception(
                 'unknown robot type: {}'.format(self.config['robot']))
