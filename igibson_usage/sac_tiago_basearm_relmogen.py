ea1f2dbc8f1f451756f6b7bc3336b07848d171cd
i-m-vivek <vivek.mittal051@gmail.com>
2022-01-25 03:19:52 +0530

    added sac for basearm

---
+++ igibson_usage/sac_tiago_basearm_relmogen.py
@@ -0,0 +1,201 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from mushroom_rl.algorithms.actor_critic import SAC
+from mushroom_rl.core import Core, Logger
+from mushroom_rl.environments.gym_env import Gym
+from mushroom_rl.utils.dataset import compute_J, parse_dataset
+from gibson2.envs.mushrl_mp_igibson_env_basearm import iGibsonMPEnv
+
+from tqdm import trange
+
+
+class CriticNetwork(nn.Module):
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
+        super().__init__()
+
+        n_input = input_shape[-1]
+        n_output = output_shape[0]
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=8, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        self._h1 = nn.Linear(64+94+7, n_features)
+        self._h2 = nn.Linear(n_features, n_features)
+        self._h3 = nn.Linear(n_features, n_output)
+
+        nn.init.xavier_uniform_(self._h1.weight, gain=nn.init.calculate_gain("relu"))
+        nn.init.xavier_uniform_(self._h2.weight, gain=nn.init.calculate_gain("relu"))
+        nn.init.xavier_uniform_(self._h3.weight, gain=nn.init.calculate_gain("linear"))
+
+    def forward(self, state, action):
+        aux_n_taskobs = state[:, :94]
+        depth = state[:, 94:].view(-1, 1, 128, 128)
+        cnn_out = torch.flatten(self.cnn(depth), 1)
+        state_action = torch.cat((aux_n_taskobs.float(), cnn_out.float(), action.float()), dim=1)
+        features1 = F.relu(self._h1(state_action))
+        features2 = F.relu(self._h2(features1))
+        q = self._h3(features2)
+
+        return torch.squeeze(q)
+
+class ActorNetwork(nn.Module):
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
+        super(ActorNetwork, self).__init__()
+
+        n_input = input_shape[-1]
+        n_output = output_shape[0]
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=8, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        self._h1 = nn.Linear(64+94, n_features)
+        self._h2 = nn.Linear(n_features, n_features)
+        self._h3 = nn.Linear(n_features, n_output)
+
+        nn.init.xavier_uniform_(self._h1.weight, gain=nn.init.calculate_gain("relu"))
+        nn.init.xavier_uniform_(self._h2.weight, gain=nn.init.calculate_gain("relu"))
+        nn.init.xavier_uniform_(self._h3.weight, gain=nn.init.calculate_gain("linear"))
+
+        # cnn init taken from HRL4IN
+        for layer in self.cnn:
+            if isinstance(layer, (nn.Conv2d, nn.Linear)):
+                nn.init.orthogonal_(layer.weight, gain=1)
+                nn.init.constant_(layer.bias, val=0)
+            
+    def forward(self, state):
+        state = state.float()
+        aux_n_taskobs = state[:, :94]
+        depth = state[:, 94: ].view(-1, 1, 128, 128)
+        cnn_out = torch.flatten(self.cnn(depth), 1)
+        out = torch.cat((aux_n_taskobs.float(), cnn_out.float()), dim=1)
+        features1 = F.relu(self._h1(out.float()))
+        features2 = F.relu(self._h2(features1))
+        a = self._h3(features2)
+        return a
+
+def experiment(alg, n_epochs, n_steps, n_steps_test):
+    np.random.seed()
+
+    logger = Logger(alg.__name__ + "_basearm", results_dir="relmogen_exps/", log_console=True)
+    logger.strong_line()
+    logger.info("Experiment Algorithm: " + alg.__name__)
+
+    # MDP
+    horizon = 200
+    gamma = 0.99
+    mdp = iGibsonMPEnv(
+        config_file="new_configs/tiago_basearm_relmogen.yaml", horizon=horizon, gamma=gamma
+    )
+
+    # Settings
+    initial_replay_size = 512
+    max_replay_size = 50000
+    batch_size = 128
+    n_features = 128
+    warmup_transitions = 256
+    tau = 0.005
+    lr_alpha = 3e-4
+
+    use_cuda = torch.cuda.is_available()
+
+    # Approximator
+    actor_input_shape = mdp.info.observation_space.shape
+    actor_mu_params = dict(
+        network=ActorNetwork,
+        n_features=n_features,
+        input_shape=actor_input_shape,
+        output_shape=mdp.info.action_space.shape,
+        use_cuda=use_cuda,
+    )
+    actor_sigma_params = dict(
+        network=ActorNetwork,
+        n_features=n_features,
+        input_shape=actor_input_shape,
+        output_shape=mdp.info.action_space.shape,
+        use_cuda=use_cuda,
+    )
+
+    actor_optimizer = {"class": optim.Adam, "params": {"lr": 3e-4}}
+
+    critic_input_shape = (actor_input_shape[0] + mdp.info.action_space.shape[0],)
+    critic_params = dict(
+        network=CriticNetwork,
+        optimizer={"class": optim.Adam, "params": {"lr": 3e-4}},
+        loss=F.mse_loss,
+        n_features=n_features,
+        input_shape=critic_input_shape,
+        output_shape=(1,),
+        use_cuda=use_cuda,
+    )
+
+    # Agent
+    agent = alg(
+        mdp.info,
+        actor_mu_params,
+        actor_sigma_params,
+        actor_optimizer,
+        critic_params,
+        batch_size,
+        initial_replay_size,
+        max_replay_size,
+        warmup_transitions,
+        tau,
+        lr_alpha,
+        critic_fit_params=None,
+    )
+
+    # Algorithm
+    core = Core(agent, mdp)
+
+    # RUN
+    dataset = core.evaluate(n_steps=n_steps_test, render=False)
+    s, *_ = parse_dataset(dataset)
+
+    J = np.mean(compute_J(dataset, mdp.info.gamma))
+    R = np.mean(compute_J(dataset))
+    s = s[:batch_size, :]
+    E = agent.policy.entropy(s)
+
+    logger.epoch_info(0, J=J, R=R, entropy=E)
+
+    core.learn(n_steps=initial_replay_size, n_steps_per_fit=initial_replay_size)
+
+    for n in trange(n_epochs, leave=False):
+        core.learn(n_steps=n_steps, n_steps_per_fit=1)
+        dataset = core.evaluate(n_steps=n_steps_test, render=False)
+        s, *_ = parse_dataset(dataset)
+
+        J = np.mean(compute_J(dataset, mdp.info.gamma))
+        R = np.mean(compute_J(dataset))
+        s = s[:batch_size, :]
+        E = agent.policy.entropy(s)
+
+        logger.epoch_info(n + 1, J=J, R=R, entropy=E)
+        logger.log_best_agent(agent, J)
+
+    logger.log_agent(agent)
+    logger.log_dataset(dataset)
+    logger.info("Experiment Terminated")
+
+if __name__ == "__main__":
+    algs = [SAC]
+
+    for alg in algs:
+        experiment(alg=alg, n_epochs=200, n_steps=1024, n_steps_test=1024)
